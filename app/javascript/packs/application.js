51ab9243f1268d1b05e53c09e41c62d71f32b491
Christian Zamora <christianzamoraburgos@gmail.com>
2021-07-21 23:17:54 -0500

    added jquery, wowjs from yarn and added import to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,21 +4,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+window.WOW = require('wowjs').WOW;
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/wow.min")
+
 
 import 'bootstrap';
 import 'bootstrap/dist/js/bootstrap';
 import "bootstrap/dist/css/bootstrap";
 
-import { initWow } from '../components/init-wow';
+// import { initWow } from '../components/init-wow';
 
-document.addEventListener('turbolinks:load', () => {
-  initWow();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   initWow();
+// });
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
